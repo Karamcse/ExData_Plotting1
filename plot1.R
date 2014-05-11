ec57e07f13989b9aac778f8ba0977bf0756eb253
Karamcse <Karamcse@github.com>
2014-05-11 10:52:10 +0530

    plot1

---
+++ plot1.R
@@ -0,0 +1,14 @@
+setwd("J:/Coursera//R//Exploratory data analysis/CorrectedCourseProject1/")
+lines <- grep('^[1-2]/2/2007', readLines('household_power_consumption.txt'))
+skipline<-min(lines)
+keeprows<-length(lines)
+data<-read.table("./household_power_consumption.txt",sep=";",skip=(skipline-1),
+                 nrow=keeprows,header=T,na.strings=c("?","NA",""),
+                 col.names=colnames(read.table("./household_power_consumption.txt",nrow = 1,header = TRUE,sep=";")))
+
+data$Date<-as.Date(data$Date,"%d/%m/%Y")
+data$DateTime<-strptime(paste(data$Date,data$Time),"%Y-%m-%d %H:%M:%S")
+par(pin=c(6.4,6.4))
+png(filename="./ExData_Plotting1//figure/plot1.png")
+hist(data$Global_active_power,col="red",xlab="Global Active Power (kilowatts)",main="Global Active Power")
+dev.off()
