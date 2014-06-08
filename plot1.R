6ea2eb682dfdc0cfc5a39dc419eb3cecdbf2f226
Karamcse <Karamcse@github.com>
2014-06-08 19:10:21 +0530

    Plot 1 created

--- plot1.R
+++ plot1.R
@@ -1,4 +1,6 @@
 source("./ExData_Plotting1/readdata.R")
+##Construct the plot and save it to a PNG file with a width of 480 pixels and a height of 480 pixels.
+
 par(pin=c(6.4,6.4))
 par(bg="transparent")
 png(filename="./ExData_Plotting1//figure/plot1.png")
