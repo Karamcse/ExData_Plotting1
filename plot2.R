bab2d477fd33e19ecd9d8c9585f486f22d0beb76
Karamcse <Karamcse@github.com>
2014-06-08 19:19:25 +0530

    Plot 2 Added

--- plot2.R
+++ plot2.R
@@ -3,7 +3,7 @@ source("./ExData_Plotting1/readdata.R")
 par(pin=c(4.5,4.5))
 ## Create a Time Series on Global Active Power
 par(bg="transparent")
-png(filename="./ExData_Plotting1/figure/plot2.png")
+png(filename="./figure/plot2.png")
 
 plot(data$Global_active_power~data$DateTime,type="l",ylab="Global Active Power (kilowatts)",xlab="")
 dev.off()
