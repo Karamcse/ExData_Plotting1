9a200faf6b9d9471a0dcfba1d1fdde506617fb03
Karamcse <Karamcse@github.com>
2014-06-08 19:16:44 +0530

    Plot 2 Added

--- plot2.R
+++ plot2.R
@@ -1,6 +1,9 @@
 source("./ExData_Plotting1/readdata.R")
+
 par(pin=c(4.5,4.5))
+## Create a Time Series on Global Active Power
 par(bg="transparent")
 png(filename="./ExData_Plotting1/figure/plot2.png")
+
 plot(data$Global_active_power~data$DateTime,type="l",ylab="Global Active Power (kilowatts)",xlab="")
 dev.off()
--- readdata.R
+++ readdata.R
@@ -1,11 +1,21 @@
-setwd("J:/Coursera//R//Exploratory data analysis/CorrectedCourseProject1/")
-lines <- grep('^[1-2]/2/2007', readLines('household_power_consumption.txt'))
+ 
+## Download the file and unzip to ProjecData Folder
+
+if(!file.exists("../ProjectData//household_power_consumption.txt"))
+{
+        download.file(url="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",destfile="../household_power_consumption.zip",method="auto")
+        unzip("../household_power_consumption.zip",exdir="../ProjectData")
+}
+
+## We will only be using data from the dates 2007-02-01 and 2007-02-02.
+lines <- grep('^[1-2]/2/2007', readLines('../household_power_consumption.txt'))
 skipline<-min(lines)
 keeprows<-length(lines)
-data<-read.table("./household_power_consumption.txt",sep=";",skip=(skipline-1),
+data<-read.table("../ProjectData/household_power_consumption.txt",sep=";",skip=(skipline-1),
                  nrow=keeprows,header=T,na.strings=c("?","NA",""),
-                 col.names=colnames(read.table("./household_power_consumption.txt",nrow = 1,header = TRUE,sep=";")))
+                 col.names=colnames(read.table("../ProjectData//household_power_consumption.txt",nrow = 1,header = TRUE,sep=";")))
 
 data$Date<-as.Date(data$Date,"%d/%m/%Y")
 data$DateTime<-strptime(paste(data$Date,data$Time),"%Y-%m-%d %H:%M:%S")
 data$DateTime<-as.POSIXct(data$DateTime)
+
